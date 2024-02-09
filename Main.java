c90e29005c68d06931bf1f1b2fabfe09c39c8f9d
Murendenimudzusi <113940379+Murendenimudzusi@users.noreply.github.com>
2024-02-09 15:43:04 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int combined(int [] combine = {11,22,33,45};  int [] combine1 ={11,22,33,45};);
+        System.out.println(Array_Integers);
+
+         public int combined(int [] combine = {11,22,33,45};  int [] combine1 ={11,22,33,45};){
+
+            int[][] Array_Integers ;
+
+         for( int i = 0 ;  i < Array_Integers.length; i++){
+
+
+             Array_Integers[0][1];
+             Array_Integers[1][0];
+             Array_Integers[0][1];
+             Array_Integers[1][0];
+             Array_Integers[1][0];
+
+             return Array_Integers;
+
+            }
+
+
+        }
+
+    }
+}
